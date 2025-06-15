6f563233b22fd6098a1008068eecdb344cf1d177
Andy Wickert <andy@northernwidget.com>
2025-06-15 10:14:05 -0500

    Add output and analysis files for the four rivers.

---
+++ Minnesota/run.197/check_result.py
@@ -0,0 +1,61 @@
+import dakota.interfacing as di
+import ottar
+import numpy as np
+import pandas as pd
+
+run_number = '301'
+
+directory = '/home/awickert/Dropbox/Papers/InProgress/ChannelWidth/dakota_minnesota/logvars/out/run.'+run_number+'/'
+
+params, results = di.read_parameters_file(parameters_file=directory+'params.in', results_file=directory+'results.out')
+
+################
+# Observations #
+################
+obs = pd.read_csv('DevonLibby_Table3-3_Reach08.csv')
+# Test data out of range
+#obs['Year'][0] = 1923
+
+###############
+# Predictions #
+###############
+rw = ottar.RiverWidth.from_yaml('config-dakota-params.yaml')
+
+rw.initialize()
+
+# Update values
+rw.set_initial_width( params['b0'] )
+rw.set_tau_crit_cohesive( params['tau_crit'] )
+rw.set_cohesive_detachment_coefficient( 10**params['log__k_d'] )
+rw.set_noncohesive_entrainment_coefficient( 10**params['log__k_E'] )
+rw.set_coehsive_bank_stickiness( 10**params['log__f_stickiness'] )
+rw.set_noncohesive_narrowing_coefficient( 10**params['log__k_n_noncohesive'] )
+
+rw.run()
+rw.finalize()
+
+
+
+# Predicted values in observed years
+# Since we don't have a known date of observation, let's use the annual mean.
+# Modeled width by year
+modeled_width_by_year = rw.df.groupby(rw.df['Timestamp'].dt.year).mean()['Channel width [m]']
+
+# Compare with data
+comp = obs.copy()
+# Empty data column
+comp['Width modeled [m]'] = np.nan
+
+for _yr in comp['Year']:
+    try:
+        comp['Width modeled [m]'][comp['Year'] == _yr] = \
+            modeled_width_by_year[_yr]
+    except:
+        comp['Width modeled [m]'][comp['Year'] == _yr] = np.nan
+
+comp = comp.dropna()
+
+comp['Width error [m]'] = comp['Width modeled [m]'] - comp['Width [m]']
+
+rmse = ( (comp['Width error [m]']**2).sum() / len(comp) )**.5
+
